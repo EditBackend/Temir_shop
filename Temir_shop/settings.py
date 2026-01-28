1febc817316f9afbc889073edd844c7b01a65c87
EditBackend <commanderx002@gmail.com>
2026-01-28 14:24:32 +0500

    Fix settings for production (DEBUG=False, Render config)

--- Temir_shop/settings.py
+++ Temir_shop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y1(gt@i9gg()mi+1iqza2n4t+7u@kwzlj8s%@+ip$l@23^lt*c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
