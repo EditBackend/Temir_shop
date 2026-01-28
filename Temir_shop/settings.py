f7af323181fbc83c0fdcbb4624f0c8999db4831f
EditBackend <commanderx002@gmail.com>
2026-01-28 16:29:49 +0500

    Update settings.py for server deployment

--- Temir_shop/settings.py
+++ Temir_shop/settings.py
@@ -105,10 +105,15 @@ WSGI_APPLICATION = 'Temir_shop.wsgi.application'
 # =======================
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("DB_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("DB_NAME", BASE_DIR / 'db.sqlite3'),
+        'USER': os.environ.get("DB_USER", ""),
+        'PASSWORD': os.environ.get("DB_PASSWORD", ""),
+        'HOST': os.environ.get("DB_HOST", ""),
+        'PORT': os.environ.get("DB_PORT", ""),
     }
 }
+
 
 
 # =======================
